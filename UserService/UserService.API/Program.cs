ef2f559fbe85bfb039d264ab446a906ce24bc34b
Rahul Dey <rahuldey9623@gmail.com>
2025-04-28 15:35:19 +0000

    Removed swagger ui and open api config

--- UserService/UserService.API/Program.cs
+++ UserService/UserService.API/Program.cs
@@ -2,20 +2,11 @@ using UserService.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddControllers();
 
 builder.Services.AddApiServices(builder.Configuration);
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.MapControllers();
 
