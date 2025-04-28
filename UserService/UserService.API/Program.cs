5a72828a0cb7753fae297ec0f5a01894fffc1c8c
Rahul Dey <rahuldey9623@gmail.com>
2025-04-28 16:43:43 +0000

    Add serilog config with seq sinks

--- UserService/UserService.API/Program.cs
+++ UserService/UserService.API/Program.cs
@@ -1,12 +1,18 @@
 using UserService.API.Extensions;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, loggerConfig) => 
+    loggerConfig.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddControllers();
 
 builder.Services.AddApiServices(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 app.MapControllers();
 
