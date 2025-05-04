4d64e10e167d780124aec995a0c2612ffd7af4c8
Rahul Dey <rahuldey9623@gmail.com>
2025-05-04 16:52:30 +0000

    Added service configurations

---
+++ OrderService/OrderService.Application/Constants/HttpClientConstants.cs
@@ -0,0 +1,15 @@
+namespace OrderService.Application.Constants;
+
+public class HttpClientConstants
+{
+    public const string BaseAddress = "BaseAddress";
+    public const string OrderPipelineName = "OrderPipeline";
+
+    public const int RequestTimeoutInSeconds = 5;
+    public const int MaxRetryCount = 3;
+    public const int RetryDelayInMilliseconds = 500;
+    public const int SamplingDurationInSeconds = 30;
+    public const double FailureRatio = 0.1;
+    public const int MinimumThroughput = 100;
+    public const int BreakDurationInSeconds = 5;
+}
---
+++ OrderService/OrderService.Application/Extensions/ServiceConfigurations.cs
@@ -0,0 +1,52 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http.Resilience;
+using OrderService.Application.Constants;
+using OrderService.Application.Handlers;
+using OrderService.Application.Services;
+using OrderService.Application.Services.Interfaces;
+using Polly;
+
+namespace OrderService.Application.Extensions;
+
+public static class ServiceConfigurations
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<AuthenticatedHttpClientHandler>();
+
+        services.AddHttpClient<ICartHttpClient, CartHttpClient>(client =>
+        {
+            client.BaseAddress = new Uri(configuration[HttpClientConstants.BaseAddress] ?? "");
+        })
+        .AddHttpMessageHandler<AuthenticatedHttpClientHandler>()
+        .AddResilienceHandler(
+            HttpClientConstants.OrderPipelineName,
+            ConfigureDefaultResiliencePipeline);
+
+        services.AddScoped<IOrdersService, OrdersService>();
+
+        return services;
+    }
+
+    private static void ConfigureDefaultResiliencePipeline(ResiliencePipelineBuilder<HttpResponseMessage> pipelineBuilder)
+    {
+        pipelineBuilder.AddTimeout(TimeSpan.FromSeconds(HttpClientConstants.RequestTimeoutInSeconds));
+
+        pipelineBuilder.AddRetry(new HttpRetryStrategyOptions
+        {
+            MaxRetryAttempts = HttpClientConstants.MaxRetryCount,
+            BackoffType = DelayBackoffType.Exponential,
+            UseJitter = true,
+            Delay = TimeSpan.FromMilliseconds(HttpClientConstants.RetryDelayInMilliseconds)
+        });
+
+        pipelineBuilder.AddCircuitBreaker(new HttpCircuitBreakerStrategyOptions
+        {
+            SamplingDuration = TimeSpan.FromSeconds(HttpClientConstants.SamplingDurationInSeconds),
+            FailureRatio = HttpClientConstants.FailureRatio,
+            MinimumThroughput = HttpClientConstants.MinimumThroughput,
+            BreakDuration = TimeSpan.FromSeconds(HttpClientConstants.BreakDurationInSeconds)
+        });
+    }
+}
