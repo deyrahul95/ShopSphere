7611ba7478b340d6b1ddeaa30186bcd25fcbfc84
Rahul Dey <rahuldey9623@gmail.com>
2025-05-07 17:11:09 +0530

    Updated product stocks in Inventory db

--- PaymentService/PaymentService.Application/Constants/RetryConstants.cs
+++ PaymentService/PaymentService.Application/Constants/RetryConstants.cs
@@ -3,5 +3,5 @@ namespace PaymentService.Application.Constants;
 public class RetryConstants
 {
     public const int FirstRetryDelayInMilliseconds = 500;
-    public const int MaxRetryCount = 3;
+    public const int MaxRetryCount = 2;
 }
