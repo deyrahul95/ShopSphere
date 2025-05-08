406362ac9cd14b3ea9dc24e8ce9ca2b9a229ea67
Rahul Dey <rahuldey9623@gmail.com>
2025-05-08 13:47:08 +0000

    Added request validation in add to cart and removed cart item

--- CartService/CartService.Application/Extensions/ServiceConfigurations.cs
+++ CartService/CartService.Application/Extensions/ServiceConfigurations.cs
@@ -1,7 +1,10 @@
 using CartService.Application.Constants;
 using CartService.Application.Handlers;
+using CartService.Application.Models;
 using CartService.Application.Services;
 using CartService.Application.Services.Interfaces;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Http.Resilience;
@@ -13,6 +16,11 @@ public static class ServiceConfigurations
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddFluentValidationAutoValidation()
+            .AddFluentValidationClientsideAdapters()
+            .AddValidatorsFromAssemblyContaining<AddToCartValidator>()
+            .AddValidatorsFromAssemblyContaining<RemovedFromCartValidator>();
+
         services.AddTransient<AuthenticatedHttpClientHandler>();
 
         services.AddHttpClient<IProductHttpClient, ProductHttpClient>(client =>
