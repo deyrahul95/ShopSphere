89af43c660a435c0d96af507ee8e6546df88e59f
Rahul Dey <rahuldey9623@gmail.com>
2025-05-08 03:34:46 +0000

    Added serilog for logging in gateway

--- GatewayService/GatewayService.Api/Program.cs
+++ GatewayService/GatewayService.Api/Program.cs
@@ -3,8 +3,15 @@ using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
 using Ocelot.Cache.CacheManager;
 using Steeltoe.Discovery.Client;
+using Serilog;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Exporter;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, loggerConfig) =>
+    loggerConfig.ReadFrom.Configuration(context.Configuration));
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
@@ -12,9 +19,25 @@ builder.Services.AddDiscoveryClient(builder.Configuration);
 
 builder.Services.AddOcelot()
     .AddConsul()
-    .AddCacheManager(x => x.WithDictionaryHandle()); ;
+    .AddCacheManager(x => x.WithDictionaryHandle());
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService("GatewayService"))
+    .WithTracing(tracing =>
+    {
+        tracing
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation()
+            .AddOtlpExporter(options =>
+            {
+                options.Endpoint = new Uri(builder.Configuration["OTLPEndpoint"] ?? "");
+                options.Protocol = OtlpExportProtocol.HttpProtobuf;
+            });
+    });
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 app.UseOcelot().Wait();
 
