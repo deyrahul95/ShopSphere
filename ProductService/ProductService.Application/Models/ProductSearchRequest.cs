b31462bbd3c1cf0579ac41ec332d70be351e8047
Rahul Dey <rahuldey9623@gmail.com>
2025-05-08 13:32:24 +0000

    Added product search validator

--- ProductService/ProductService.Application/Models/ProductSearchRequest.cs
+++ ProductService/ProductService.Application/Models/ProductSearchRequest.cs
@@ -1,12 +1,34 @@
-using System.ComponentModel.DataAnnotations;
+using FluentValidation;
 using ProductService.Application.Constants;
 
 namespace ProductService.Application.Models;
 
 public record ProductSearchRequest(
-    [Range(ValidationConstants.MinPageNumber, ValidationConstants.MaxPageNumber)] int? PageNumber = null,
-    [Range(ValidationConstants.MinPageSize, ValidationConstants.MaxPageSize)] int? PageSize = null,
-    [MaxLength(ValidationConstants.MaxNameLength)] string? Name = null,
-    [MaxLength(ValidationConstants.MaxCategoryLength)] string? Category = null,
+    int? PageNumber = null,
+    int? PageSize = null,
+    string? Name = null,
+    string? Category = null,
     string? Sort = null
 );
+
+public class ProductSearchValidator : AbstractValidator<ProductSearchRequest>
+{
+    public ProductSearchValidator()
+    {
+        RuleFor(x => x.PageNumber)
+            .InclusiveBetween(ValidationConstants.MinPageNumber, ValidationConstants.MaxPageNumber)
+            .WithMessage($"Page number must be in between {ValidationConstants.MinPageNumber} and {ValidationConstants.MaxPageNumber}");
+
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(ValidationConstants.MinPageSize, ValidationConstants.MaxPageSize)
+            .WithMessage($"Page size must be in between {ValidationConstants.MinPageSize} and {ValidationConstants.MaxPageSize}");
+
+        RuleFor(x => x.Name)
+            .MaximumLength(ValidationConstants.MaxNameLength)
+            .WithMessage($"Name can't be more than {ValidationConstants.MaxNameLength} characters long");
+
+        RuleFor(x => x.Category)
+            .MaximumLength(ValidationConstants.MaxCategoryLength)
+            .WithMessage($"Category can't be more than {ValidationConstants.MaxCategoryLength} characters long");
+    }
+}
