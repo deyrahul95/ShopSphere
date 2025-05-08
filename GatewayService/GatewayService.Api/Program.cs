efabfd133aba17d4ce0c583212f4f985878fe1a1
Rahul Dey <rahuldey9623@gmail.com>
2025-05-08 03:28:33 +0000

    Added api gateway using ocelot and consul for service discovery

--- GatewayService/GatewayService.Api/Program.cs
+++ GatewayService/GatewayService.Api/Program.cs
@@ -1,6 +1,21 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Ocelot.Cache.CacheManager;
+using Steeltoe.Discovery.Client;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddDiscoveryClient(builder.Configuration);
+
+builder.Services.AddOcelot()
+    .AddConsul()
+    .AddCacheManager(x => x.WithDictionaryHandle()); ;
 
 var app = builder.Build();
 
-await app.RunAsync();
+app.UseOcelot().Wait();
 
+await app.RunAsync();
