ffefc192d6b89fd4c883d897547707f066a19a20
Rahul Dey <rahuldey9623@gmail.com>
2025-07-25 03:36:24 +0000

    ☑️ Updated clean up script to remove repo images

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,8 @@
 
 # Configuration
 VERSION_FILE=".version"
-PREFIX="shopsphare"
+IMAGE_PREFIX="shopsphare"
+REPO_PREFIX="deyrahul95"
 
 # Read or initialize version
 if [ ! -f "$VERSION_FILE" ]; then
@@ -23,7 +24,20 @@ CURRENT_VERSION=$(cat "$VERSION_FILE")
 SERVICES=$(docker-compose config --services)
 for SERVICE in $SERVICES; do
     IMAGE_SERVICE_NAME="${SERVICE//_/-}"
-    OLD_IMAGE="$PREFIX-$IMAGE_SERVICE_NAME:$CURRENT_VERSION"
+    
+    OLD_IMAGE="$IMAGE_PREFIX-$IMAGE_SERVICE_NAME:$CURRENT_VERSION"
+    if docker image inspect "$OLD_IMAGE" > /dev/null 2>&1; then
+        echo "🗑️ Removing old image: $OLD_IMAGE"
+        docker rmi "$OLD_IMAGE"
+    fi
+
+    OLD_IMAGE="$REPO_PREFIX/$IMAGE_PREFIX-$IMAGE_SERVICE_NAME:$CURRENT_VERSION"
+    if docker image inspect "$OLD_IMAGE" > /dev/null 2>&1; then
+        echo "🗑️ Removing old image: $OLD_IMAGE"
+        docker rmi "$OLD_IMAGE"
+    fi
+
+    OLD_IMAGE="$REPO_PREFIX/$IMAGE_PREFIX-$IMAGE_SERVICE_NAME:latest"
     if docker image inspect "$OLD_IMAGE" > /dev/null 2>&1; then
         echo "🗑️ Removing old image: $OLD_IMAGE"
         docker rmi "$OLD_IMAGE"
