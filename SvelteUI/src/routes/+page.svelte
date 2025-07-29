35c3c2968e8b22a548e63cf2ff14e06b1cfb2ecf
Rahul Dey <rahuldey9623@gmail.com>
2025-07-29 01:51:30 +0000

    Added shadcn into our app

--- SvelteUI/src/routes/+page.svelte
+++ SvelteUI/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Button from "$lib/components/ui/button/button.svelte";
+
+</script>
+
+<main>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    
+    <Button>Click Me</Button>
+</main>
